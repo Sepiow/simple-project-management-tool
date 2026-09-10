7e818e8b07705e6ec85da687b55b4260a457163b
Sepiow <larryjosefcuenca@gmail.com>
2026-09-10 20:23:50 +0800

    User settings fix

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,32 +1,16 @@
-import { redirect } from "next/navigation"
 import { getCurrent } from "@/features/auth/queries"
-import { getProject } from "@/features/projects/queries"
-import { EditProjectForm } from "@/features/projects/components/edit-project-form"
+import { redirect } from "next/navigation"
+import { EditProfileCard } from "@/features/auth/components/edit-profile-card"
 
 export const dynamic = "force-dynamic"
 
-interface ProjectIdSettingsPageProps {
-  params: Promise<{
-    projectId: string
-  }>
-}
-
-export default async function ProjectIdSettingsPage({
-  params
-}: ProjectIdSettingsPageProps) {
+export default async function SettingsPage() {
   const user = await getCurrent()
   if (!user) redirect("/sign-in")
 
-  const { projectId } = await params
-  const initialValues = await getProject({ projectId })
-
-  if (!initialValues) {
-    redirect(`/projects/${projectId}`)
-  }
-
   return (
-    <div className="w-full lg:max-w-xl">
-      <EditProjectForm initialValues={initialValues} />
+    <div className="w-full lg:max-w-xl mx-auto py-6">
+      <EditProfileCard />
     </div>
   )
 }
